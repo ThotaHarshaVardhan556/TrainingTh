a7444e7b0c692633a4501487620702516d11e19c
ThotaHarshaVardhan556 <157490375+ThotaHarshaVardhan556@users.noreply.github.com>
2025-01-28 19:37:22 +0530

    Add files via upload

--- dom.js
+++ dom.js
@@ -68,50 +68,95 @@
 
 // })
 // Create and style the main container
-let mainEle = document.createElement("div");
-mainEle.setAttribute("id", "mainBlock");
-mainEle.style.border = "2px solid red";
-mainEle.style.width = "550px";
-mainEle.style.height = "550px";
+// let mainEle = document.createElement("div");
+// mainEle.setAttribute("id", "mainBlock");
+// mainEle.style.border = "2px solid red";
+// mainEle.style.width = "550px";
+// mainEle.style.height = "550px";
 
 
-let topEle = document.createElement("div");
-topEle.setAttribute("class", "topBlock");
-topEle.style.textAlign = "center";
-topEle.style.padding = "10px";
-topEle.textContent = "Welcome to the Cat Gallery";
+// let topEle = document.createElement("div");
+// topEle.setAttribute("class", "topBlock");
+// topEle.style.textAlign = "center";
+// topEle.style.padding = "10px";
+// topEle.textContent = "Welcome to the Cat Gallery";
 
 
-let image = document.createElement("img");
-image.src = "https://cdn.pixabay.com/photo/2023/02/09/16/43/cat-7779237_640.jpg";
-image.style.width = "550px";
-image.style.display = "block";
-image.style.margin = "auto";
+// let image = document.createElement("img");
+// image.src = "https://cdn.pixabay.com/photo/2023/02/09/16/43/cat-7779237_640.jpg";
+// image.style.width = "550px";
+// image.style.display = "block";
+// image.style.margin = "auto";
 
-let h1 = document.createElement("h1");
-h1.innerText = "CAT";
-h1.style.textAlign="center"
-
-
-let btn = document.createElement("button");
-h1.innerText = "View More";
-btn.style.border="none";
-btn.style.padding="20px";
-btn.style.backgroundColor="dodgreblue";
-btn.style.color="White";
-h1.style.textAlign="center"
-
-let bottomEle = document.createElement("div");
-bottomEle.setAttribute("class", "bottomBlock");
-bottomEle.style.textAlign = "center";
-bottomEle.style.padding = "10px";
-bottomEle.textContent = "Enjoy this cute cat!";
-
-bottomEle.appendChild(h1)
-bottomEle.appendChild(btn)
-mainEle.appendChild(topEle);
-mainEle.appendChild(image);
-mainEle.appendChild(bottomEle);
+// let h1 = document.createElement("h1");
+// h1.innerText = "CAT";
+// h1.style.textAlign="center"
 
 
-document.body.appendChild(mainEle);
+// let btn = document.createElement("button");
+// h1.innerText = "View More";
+// btn.style.border="none";
+// btn.style.padding="20px";
+// btn.style.backgroundColor="dodgreblue";
+// btn.style.color="White";
+// h1.style.textAlign="center"
+
+// let bottomEle = document.createElement("div");
+// bottomEle.setAttribute("class", "bottomBlock");
+// bottomEle.style.textAlign = "center";
+// bottomEle.style.padding = "10px";
+// bottomEle.textContent = "Enjoy this cute cat!";
+
+// bottomEle.appendChild(h1)
+// bottomEle.appendChild(btn)
+// mainEle.appendChild(topEle);
+// mainEle.appendChild(image);
+// mainEle.appendChild(bottomEle);
+
+
+// document.body.appendChild(mainEle);
+let form = document.querySelector("form");
+
+let username = document.getElementById("uName");
+let password = document.getElementById("uPass");
+let check = document.getElementById("check")
+let show = document.getElementById("show")
+
+let gender = document.getElementsByName("gender")
+
+
+check.addEventListener("click",event=>{
+    if(event.target.checked == true){
+        password.setAttribute("type","text");
+        show.innerText = "hide Password";
+        
+        }else{
+            password.setAttribute("type" , "password");
+            showinnerText="showpassword"
+
+    }
+})
+
+form.addEventListener("submit" , event=>{
+    event.preventDefault();
+    let un = username.value;
+    let up = password.value;
+    let gen = "";
+
+    for(let i=0;i<=gender.length-1;i++){
+        if(gender[i].checked == true){
+            gen = gender[i].value;
+        }
+    }
+
+
+    let userDetails={
+        username:un,
+        password:up,
+        gender:gen
+    }
+    console.log(userDetails);
+    sessionStorage.setItem("userData" ,
+        JSON.stringify(userDetails)
+    )
+})
